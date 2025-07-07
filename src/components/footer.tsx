b6781b6107e3729bf1b39784b404dc737defe27c
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-07 17:04:57 +0700

    feat: create footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className='border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12'>
+      <div className='container mx-auto px-4 text-center text-gray-400'>
+        <p>Made with 💗 by Muhammad Yusuf Untung Wahyudi</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
