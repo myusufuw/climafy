c4ecf4bb1fabc5f281633ec2a6715b8baaa6c2b6
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-07 20:27:37 +0700

    feat: create header component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,11 @@
 import { type PropsWithChildren } from 'react'
 import Footer from './footer'
+import Header from './header'
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='bg-gradient-to-br from-background to-muted'>
-      header
+    <div className='bg-gradient-to-br from-background to-violet-950'>
+      <Header />
       <main className='min-h-screen container mx-auto px-4 py-8'>
         {children}
       </main>
