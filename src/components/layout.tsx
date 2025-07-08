8039d3c4d8d634c9a28114cae1d054c2a3bf8f78
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-08 12:27:33 +0700

    style: change layout bg-color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import Header from './header'
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='bg-gradient-to-br from-background to-violet-950'>
+    <div className='bg-gradient-to-br dark:from-background from-blue-400 dark:to-violet-950 to-violet-200 transition-colors duration-500'>
       <Header />
       <main className='min-h-screen container mx-auto px-4 py-8'>
         {children}
