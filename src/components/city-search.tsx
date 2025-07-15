59248db0773e9fca26b6714fa8058e05bea92225
myusufuw <103932140+yusufvalid@users.noreply.github.com>
2025-07-15 16:21:21 +0700

    reafctor(city-search): render favorite-cities

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -1,6 +1,7 @@
+import { useFavorites } from '@/hooks/use-favorite'
 import { useSearchHistory } from '@/hooks/use-search-history'
 import { useLocationSearch } from '@/hooks/use-weather'
-import { Clock, Loader2, Search, XCircle } from 'lucide-react'
+import { Clock, Loader2, Search, Star, XCircle } from 'lucide-react'
 import moment from 'moment'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -22,6 +23,7 @@ const CitySearch = () => {
 
   const { data: locations, isLoading } = useLocationSearch(query)
   const { history, addToHistory, clearHistory } = useSearchHistory()
+  const { favorites } = useFavorites()
 
   const handleSelect = (cityData: string) => {
     const [lat, lon, name, country] = cityData.split('|')
@@ -42,7 +44,7 @@ const CitySearch = () => {
     <>
       <Button
         variant='outline'
-        className='relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64'
+        className='relative justify-start text-sm text-muted-foreground w-fit'
         onClick={() => setOpenDialogSearch(true)}
       >
         <Search className='mr-2 h-4 w-4' />
@@ -59,10 +61,30 @@ const CitySearch = () => {
           {query.length > 2 && !isLoading && (
             <CommandEmpty>No cities found.</CommandEmpty>
           )}
+
           {/* FAVORITES */}
-          <CommandGroup heading='Favorites'>
-            <CommandItem>Calendar</CommandItem>
-          </CommandGroup>
+          {favorites.length > 0 && (
+            <CommandGroup heading='Favorites'>
+              {favorites.map((city) => (
+                <CommandItem
+                  key={city.id}
+                  value={`${city.lat}|${city.lon}|${city.name}|${city.country}`}
+                  onSelect={handleSelect}
+                >
+                  <Star className='mr-2 h-4 w-4 text-yellow-500' />
+                  <span>{city.name}</span>
+                  {city.state && (
+                    <span className='text-sm text-muted-foreground'>
+                      , {city.state}
+                    </span>
+                  )}
+                  <span className='text-sm text-muted-foreground'>
+                    , {city.country}
+                  </span>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          )}
 
           {/* RECENTS */}
           {history.length > 0 && (
